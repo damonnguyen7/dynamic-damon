ca624d96b97a511ae9e943a0eac413b0a2c458a6
Damon <damonnguyen92@gmail.com>
2017-01-23 13:33:00 -0800

    Put dummy data for blog for styling

--- routes/blog.js
+++ routes/blog.js
@@ -6,5 +6,8 @@ router.get('/', function(req, res) {
   res.render('blog-list', {title: 'list of blogs'});
 });
 
+router.get('/detail', function(req, res) {
+  res.render('blog-list-item-detail', {title: 'Blog Detail'});
+});
 
 module.exports = router;
